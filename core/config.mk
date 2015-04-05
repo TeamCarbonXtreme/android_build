442a298ff2024674fc0dafd2d2fb847c3c3a6011
Nothing-Dev <alfredobigpapi2@gmail.com>
2015-04-05 11:39:14 -0430

    Revert "CXR_BUILD (1/2)"
    This reverts commit aab79391d466a373971431eac4e1e4cef5076054.

--- core/config.mk
+++ core/config.mk
@@ -501,7 +501,7 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
-ifneq ($(CXR_BUILD),)
+ifneq ($(CM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/cm/sepolicy/sepolicy.mk)
--- core/product_config.mk
+++ core/product_config.mk
@@ -179,9 +179,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A CXR build needs only the CXR product makefiles.
-ifneq ($(CXE_BUILD),)
-  all_product_configs := $(shell ls device/*/$(CXR_BUILD)/cxr.mk)
+# A CM build needs only the CM product makefiles.
+ifneq ($(CM_BUILD),)
+  all_product_configs := $(shell ls device/*/$(CM_BUILD)/cm.mk)
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -192,9 +192,9 @@ else
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # CXR_BUILD
+endif # CM_BUILD
 
-ifeq ($(CXR_BUILD),)
+ifeq ($(CM_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
